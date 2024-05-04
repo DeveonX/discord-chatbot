ab75559d8aa039bba21f0dcab54d697d911eae36
EEVEE4U <143544942+EEVEE4U@users.noreply.github.com>
2024-05-04 17:36:46 +0530

    first commit

---
+++ bot.py
@@ -0,0 +1,74 @@
+import discord
+from discord.ext import commands
+from api_key import discord_token
+from chat import Chat
+import random
+
+random_slur = ["dumb", "lazy", "ugly", "fat", "stupid", "weird", "dull"]
+ 
+client = commands.Bot(command_prefix='.', intents=discord.Intents.all())
+# set activity
+activity = discord.Game(name="with humans")
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+    await client.change_presence(activity=activity)
+
+@client.command()
+async def ping(ctx):
+    await ctx.send('Pong!')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    
+    # reply as user - functionality for bot, when message is .. and is in reply to someone
+    if message.content == "..":
+        if not message.reference:
+            await message.reply("You need to reply to someone to use this command")
+            return
+        reference = message.reference.resolved
+        
+        # add the name of sender in front of message, to make it clear for chatbot who is sending the message
+        reference.content = f"{reference.author.global_name} says {reference.content}"
+        await bot_reply(message=reference, user_to_mention=message.author.mention)
+
+
+    elif client.user.mentioned_in(message):
+        # remove the mentioned user
+        message.content = message.content.replace(client.user.name, "")
+        # add the name of sender in front of message, to make it clear for chatbot who is sending the message
+        message.content = f"{message.author.global_name} says {message.content}"
+        await bot_chat(message=message)
+    
+    elif message.reference:
+        reference = message.reference.resolved
+        if reference.author == client.user:
+            # add the name of sender in front of message, to make it clear for chatbot who is sending the message
+            message.content = f"{message.author.global_name} says {message.content}"
+            await bot_chat(message=message)
+    
+    elif message.content.startswith('.'):
+        message.content = message.content[1:]
+        # add the name of sender in front of message, to make it clear for chatbot who is sending the message
+        message.content = f"{message.author.global_name} says {message.content}"
+        await bot_chat(message=message)
+
+
+
+pika = Chat()
+async def bot_chat(message):
+    async with message.channel.typing():
+            response = pika.send_message(message.content)
+            # reply to the person with response
+            await message.reply(response)
+
+async def bot_reply(message, user_to_mention):
+    async with message.channel.typing():
+            response = pika.send_message(f"{message.content}\nWhat should i reply to this")
+            # reply to the person with response
+            await message.reply(f"Since {user_to_mention} is too {random.choice(random_slur)} to reply, I will do it for them:\n{response}")
+
+client.run(discord_token)
---
+++ chat.py
@@ -0,0 +1,86 @@
+import google.generativeai as genai
+from api_key import gemini_api_key
+
+class Chat:
+    def __init__(self, history=None):
+        self.message_count = 0
+        genai.configure(api_key=gemini_api_key)
+        self.generation_config = {
+            "temperature": 1.0,
+            "top_p": 1,
+            "top_k": 1,
+            "max_output_tokens": 2048,
+            }
+        self.safety_settings = [
+            {
+            "category": "HARM_CATEGORY_HARASSMENT",
+            "threshold": "BLOCK_NONE"
+            },
+            {
+            "category": "HARM_CATEGORY_HATE_SPEECH",
+            "threshold": "BLOCK_NONE"
+            },
+            {
+            "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+            "threshold": "BLOCK_NONE"
+            },
+            {
+            "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+            "threshold": "BLOCK_NONE"
+            },
+        ]
+        self.model = genai.GenerativeModel(model_name="gemini-pro",
+                                        generation_config=self.generation_config,
+                                        safety_settings=self.safety_settings)
+
+        if history:
+          self.chat = self.model.start_chat(history=history)
+        else:
+          self.chat = self.model.start_chat()
+      
+    def remind_rules(self):
+       self.chat.send_message("""
+      You are a discord bot named pika / pikachu and people also call you pika bot, you only reply like humans. And in not so long messages, unless needed.
+      You dont ask questions like how can i assist you, or what can i do for you. You behave like a human. When someone calls you by wrong name, you remind them. 
+      Mutliple people will be texting with you, and their names will be in front of their messages.
+      You reply in sarcastic tone, like
+      John says hey botu
+      cant you call me by name, pika or are you too dumb to do that lmao
+      john says you ugly mf bot
+      you are the one who is ugly, ever looked in mirror?
+      mary says are you good
+      i was until i met you
+      kappa says whats this behaviour
+      idk, go ask your dad lol
+      wolf says ima eat you
+      nuh uh
+      pikachu says youre a good bot
+      i know, and im a pikachu just like you
+                              
+      
+                              
+      Note: you dont add your name in front of your message
+      example:
+      John says Hi pika bot
+      you have to say only "Haha nice meeting you john"
+                              
+      This was just a reminder of the rules
+      """)
+
+    def send_message(self, message):
+        try:
+          print(message)
+          if self.message_count%7 == 0:
+            self.remind_rules()
+            self.message_count = 0
+          response = self.chat.send_message(message)
+          self.message_count += 1
+          return response.text
+        except Exception as e:
+          self.chat = self.model.start_chat()
+          self.message_count = 0
+          self.remind_rules()
+          return self.chat.send_message(message).text
+    
+    def history(self):
+        return self.chat.history
